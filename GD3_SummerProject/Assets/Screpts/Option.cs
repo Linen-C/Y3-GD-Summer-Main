ea77fe08e85e7980a2091990800667e2680b6bd7
Linen-C <2016002@s.asojuku.ac.jp>
2022-07-10 22:41:47 +0900

    202207-10-2

--- GD3_SummerProject/Assets/Screpts/Option.cs
+++ GD3_SummerProject/Assets/Screpts/Option.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Option : MonoBehaviour
 {
     [SerializeField] Canvas _Main;
     [SerializeField] Canvas _Option;
 
+    [SerializeField] AudioCTRL _AudioCTRL;
+    [SerializeField] float _volume;
+    [SerializeField] Slider _SEslider;
+
     public void Option_to_Main()
     {
         _Option.enabled = false;
         _Main.enabled = true;
+    }
+
+    public void CloseOption()
+    {
+        if (_AudioCTRL = null)
+        {
+            _AudioCTRL.defVolume = _volume;
+        }
+
+        Destroy(gameObject);
     }
 }
